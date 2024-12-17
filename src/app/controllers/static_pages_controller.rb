ec09cbad82c8172b2e4fc383043c866a88f2ea02
aoki-ryusei <r.aoooki27@gmail.com>
2024-12-18 01:31:06 +0900

    section5 5.3まで

--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @title = __method__.capitalize
   end
 
   def help
-    @title = __method__.capitalize
   end
 
   def about
-    @title = __method__.capitalize
+  end
+
+  def contact
   end
 end
