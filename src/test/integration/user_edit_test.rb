bb962caea81ae688bafc3761587f49f01010107c
aoki-ryusei <r.aoooki27@gmail.com>
2025-01-04 13:57:06 +0900

    section10

--- src/test/integration/user_edit_test.rb
+++ src/test/integration/user_edit_test.rb
@@ -54,6 +54,16 @@ class UserEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "Webからadmin権限が変更されないこと" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.admin?
+  end
+
   test "別のユーザーの編集画面に遷移しないこと" do
     log_in_as(@other_user)
     get edit_user_path(@user)
@@ -84,5 +94,22 @@ class UserEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
+  end
+
+  test "ログインしていない際に削除処理にアクセスできないこと" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "管理者権限でなければ削除処理にアクセスできないこと" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
   end
 end
